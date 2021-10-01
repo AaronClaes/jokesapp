8f9b15430a9a883b552f714a1c4c829c56eda5da
Aaron Claes <aaronclaes22@gmail.com>
2021-10-01 19:31:55 +0200

    feat: add joke component

---
+++ src/components/JokeCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+export const JokeCard = ({ joke }) => {
+  return (
+    <Card style={{ height: "100%" }}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {joke.category}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {joke.type === "single" ? joke.joke : joke.setup}
+          </Typography>
+          {joke.type !== "single" ? (
+            <Typography variant="body2" color="text.secondary">
+              joke.delivery
+            </Typography>
+          ) : null}
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default JokeCard;
--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,7 +1,51 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+// MUI Components
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Components
+import JokeCard from "./JokeCard";
 
 const Jokes = () => {
-  return <div>JOKES ON YOU BOYY</div>;
+  const [jokes, setJokes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchJokes = async () => {
+    const res = await axios.get("https://v2.jokeapi.dev/joke/Any?amount=10");
+    setJokes(res.data.jokes);
+    setLoading(false);
+    console.log(res.data.jokes);
+  };
+
+  useEffect(() => {
+    fetchJokes();
+  }, []);
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Container fixed>
+        {!loading ? (
+          <Grid
+            container
+            spacing={{ xs: 2 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+            alignItems="stretch"
+          >
+            {jokes.map((joke, index) => {
+              return (
+                <Grid item xs={4} sm={4} md={4} key={index}>
+                  <JokeCard joke={joke} />
+                </Grid>
+              );
+            })}
+          </Grid>
+        ) : null}
+      </Container>
+    </Box>
+  );
 };
 
 export default Jokes;
